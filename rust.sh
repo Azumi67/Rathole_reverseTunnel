02cbedb7ebec2e4f33209acc715c0c64e43010b0
Azumi <119934376+Azumi67@users.noreply.github.com>
2024-01-03 03:20:27 -0800

    Add files via upload

---
+++ rust.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+echo "This script must be run as root."
+exit 1
+fi
+apt update -y
+apt install rustc -y
+apt install cargo -y
+apt-get install pkg-config libssl-dev -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source "$HOME/.cargo/env"
+

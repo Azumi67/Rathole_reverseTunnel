a51c96d50fcea490643a1df09e78f2e9bd63b2b8
Azumi <119934376+Azumi67@users.noreply.github.com>
2024-01-04 01:10:21 -0800

    Create rust2.sh

---
+++ rust2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+echo "This script must be run as root."
+exit 1
+fi
+apt update -y
+sudo apt-get install build-essential -y
+apt-get install pkg-config libssl-dev -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source "$HOME/.cargo/env"

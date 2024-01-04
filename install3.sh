447da53a3abcbbd139cce6d92c3fbf002b67d7fc
Azumi <119934376+Azumi67@users.noreply.github.com>
2024-01-04 01:13:21 -0800

    Create install3.sh

---
+++ install3.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+    echo "Run script as root, sudo -i."
+    exit 1
+fi
+sudo apt update -y
+
+architecture=$(uname -m)
+if [ "$architecture" = "x86_64" ]; then
+    if [ ! -f "go1.21.5.linux-amd64.tar.gz" ]; then
+        wget https://github.com/Azumi67/UDP2RAW_FEC/releases/download/go/go1.21.5.linux-amd64.tar.gz
+        sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+    fi
+elif [ "$architecture" = "aarch64" ]; then
+    if [ ! -f "go1.21.5.linux-arm64.tar.gz" ]; then
+        wget https://github.com/Azumi67/UDP2RAW_FEC/releases/download/go/go1.21.5.linux-arm64.tar.gz
+        sudo tar -C /usr/local -xzf go1.21.5.linux-arm64.tar.gz
+    fi
+else
+    echo "Unsupported arch: $architecture"
+    exit 1
+fi
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
+
+source ~/.bash_profile
+go mod init mymodule
+go mod tidy
+
+if [ -f "install3.go" ]; then
+    rm install3.go
+    echo "deleted previous version!"
+fi
+
+wget https://raw.githubusercontent.com/Azumi67/Rathole_reverseTunnel/main/install3.go
+
+go run install3.go
